24d8fd6a9fa24e86de355964bcdb2e9dbd6b8a53
yosefinaagst <yosefnaagustine@gmail.com>
2026-09-13 15:22:26 +0700

    menambahkan folder ui

---
+++ Tugas Compose UI Halaman Profil/app/src/main/java/com/example/ui_profile/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.ui_profile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.ui_profile.ui.theme.UI_ProfileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UI_ProfileTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+
+                    Profile(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
