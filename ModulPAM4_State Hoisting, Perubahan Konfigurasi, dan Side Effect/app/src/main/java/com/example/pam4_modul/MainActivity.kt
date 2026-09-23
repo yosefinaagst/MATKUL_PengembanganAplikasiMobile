8104bb915ffb5c06b6b20549895bc35c79283465
yosefinaagst <yosefnaagustine@gmail.com>
2026-09-23 21:42:23 +0700

    aktivitas percobaan pertama

--- ModulPAM4_State Hoisting, Perubahan Konfigurasi, dan Side Effect/app/src/main/java/com/example/pam4_modul/MainActivity.kt
+++ ModulPAM4_State Hoisting, Perubahan Konfigurasi, dan Side Effect/app/src/main/java/com/example/pam4_modul/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pam4_modul.ui.theme.PAM4_ModulTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +26,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PAM4_ModulTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            CounterScreen(modifier = Modifier.padding(32.dp))
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PAM4_ModulTheme {
-        Greeting("Android")
+fun CounterScreen(modifier: Modifier) {
+    var number by remember { mutableStateOf(0) }
+    val label by remember { mutableStateOf("Increment") }
+    Column(modifier) {
+        Text(text = "$number", fontSize = 72.sp)
+        Button(onClick = { number += 2 }) {
+            Text(text = "$label")
+        }
     }
 }
