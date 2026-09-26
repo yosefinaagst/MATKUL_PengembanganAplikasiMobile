f382f29bb95ed242b888aa55b10cb5845a7bf4e8
yosefinaagst <yosefnaagustine@gmail.com>
2026-09-26 13:41:24 +0700

    menambahkan percobaan ke-3

--- ModulPAM4_State Hoisting, Perubahan Konfigurasi, dan Side Effect/app/src/main/java/com/example/pam4_modul/MainActivity.kt
+++ ModulPAM4_State Hoisting, Perubahan Konfigurasi, dan Side Effect/app/src/main/java/com/example/pam4_modul/MainActivity.kt
@@ -9,11 +9,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,27 +30,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            var number by remember { mutableStateOf(1) }
+            PAM4_ModulTheme {
+                var number by rememberSaveable { mutableStateOf(1) }
 
-            CounterScreen(
-                modifier = Modifier.padding(32.dp),
-                number = number,
-                label = "Double",
-                onButtonClick = { number *= 2 }
-            )
+                val snackbarHostState = remember { SnackbarHostState() }
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    snackbarHost = {
+                        SnackbarHost(hostState = snackbarHostState)
+                    }
+                ) { innerPadding ->
+
+                    CounterScreen(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .padding(32.dp),
+                        number = number,
+                        label = "Double",
+                        onButtonClick = { number *= 2 }
+                    )
+
+                    LaunchedEffect(number) {
+                        snackbarHostState.showSnackbar(
+                            "Number $number is shown!"
+                        )
+                    }
+                }
+            }
         }
     }
 }
 
 @Composable
-fun CounterScreen(modifier: Modifier,
-                  number: Int, label: String, onButtonClick: () -> Unit) {
-    var number by remember { mutableStateOf(0) }
-    val label by remember { mutableStateOf("Increment") }
+fun CounterScreen(
+    modifier: Modifier,
+    number: Int,
+    label: String,
+    onButtonClick: () -> Unit
+) {
     Column(modifier) {
         Text(text = "$number", fontSize = 72.sp)
+
         Button(onClick = onButtonClick) {
-            Text(text = "$label")
+            Text(text = label)
         }
     }
 }
+
